ae82c1416b65f25897092622e09fa7ea0daa668a
Arthur <arthur.p.wright@gmail.com>
2015-05-12 18:03:34 -0400

    Added view for projects#index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,20 @@
 class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = current_user.owned_projects.build(project_params)
+    if @project.save
+      redirect_to projects_url
+    else
+      render 'new'
+    end
+  end
+  
 end
