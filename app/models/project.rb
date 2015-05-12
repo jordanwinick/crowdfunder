a237491a652905f11a6c216f16693285eae05af6
Arthur <arthur.p.wright@gmail.com>
2015-05-12 16:54:15 -0400

    Added associations to models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ActiveRecord::Base
+  has_many :rewards
+  has_many :pledges
+
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512193217) do
+ActiveRecord::Schema.define(version: 20150512204950) do
 
   create_table "pledges", force: :cascade do |t|
     t.integer  "user_id"
@@ -34,7 +34,10 @@ ActiveRecord::Schema.define(version: 20150512193217) do
     t.datetime "end_date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "projects", ["user_id"], name: "index_projects_on_user_id"
 
   create_table "rewards", force: :cascade do |t|
     t.integer  "project_id"
