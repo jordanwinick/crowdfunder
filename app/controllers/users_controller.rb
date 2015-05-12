4ca6cd33ef5b5ba40d0d5d10d5c6fd7dd0680e73
Arthur <arthur.p.wright@gmail.com>
2015-05-12 17:07:07 -0400

    Updates User model to include validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to projects_url
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :avatar)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
+  validates :password, length: {minimum: 6}
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
+  validates :email, uniqueness: true  
+
   has_many :owned_projects, class_name: 'Project'
   has_many :backed_projects, :through => :pledges, class_name: 'Project'
   has_many :pledges
