7b08f34b57e3b7b2cabff31bb83adacfbf67a8ed
Arthur <arthur.p.wright@gmail.com>
2015-05-13 17:37:47 -0400

    Did some time formatting

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,5 +4,23 @@ class Project < ActiveRecord::Base
 
   belongs_to :user
 
+  scope :expired, -> { where("end_date < ?", Time.now) }
+  scope :active, -> { where("end_date >= ?", Time.now) }
+
   accepts_nested_attributes_for :rewards, :reject_if => :all_blank, :allow_destroy => true
+
+  def expired?
+    end_date < Time.now
+  end
+
+  def active?
+    end_date >= Time.now
+  end
+
+  # def time_left
+  #   from_time = Time.now
+  #   time_ago_in_words(from_time, end_date)
+  # end
+# link_to_if project.active?, project.name, project_path(project)
+
 end
