3c851cd9e1b8da6259a5fc60b0e7b3625e64c642
Arthur <arthur.p.wright@gmail.com>
2015-05-15 16:23:07 -0400

    Fixed some views

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
 
   root 'projects#index'
-  
+
   resources :users
   resources :projects
   resources :user_sessions, :only => [:new, :create, :destroy]
   resources :rewards, :only => [:new, :create, :show]
-  
+  resources :pledges, :only => [:create]
+
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
-  
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
